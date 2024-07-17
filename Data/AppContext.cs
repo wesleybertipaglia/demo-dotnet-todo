30f66d4cdac259ef174f41358455c43bec7c1fd7
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-17 13:29:07 -0300

    feat: add AppContext

---
+++ Data/AppContext.cs
@@ -0,0 +1,17 @@
+namespace todo.Data;
+
+using Microsoft.EntityFrameworkCore;
+using todo.Models;
+
+public class AppContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Database.sqlite");
+        optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        base.OnConfiguring(optionsBuilder);
+    }
+}
